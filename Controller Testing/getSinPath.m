8de613e894e217f1c17534fbee30c553216ecf8f
Matthew Silverman <mesilverman@wpi.edu>
2016-12-14 12:52:38 -0500

    i like turtles

--- Controller Testing/getSinPath.m
+++
@@ -1,8 +0,0 @@
-function [x,y,phi] = getSinPath(x, a, w)
-
-x = 0:.001:x;
-y = a*sin(w*x);
-slope = cos(x);
-phi = atan(slope);
-
-end
--- Controller Testing/thetaCap.m
+++
@@ -1,7 +0,0 @@
-function theta = thetaCap(theta)
-
-if( theta < -pi )
-    theta = theta+2*pi;
-elseif( theta > pi )
-    theta = theta-2*pi;
-end
